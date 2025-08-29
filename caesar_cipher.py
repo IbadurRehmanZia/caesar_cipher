ec74fc54b6950a270f5c9b2aa9187cb1cf004135
unknown <ibadzia3404@gmail.com>
2025-08-29 03:28:42 +0200

    basic structure

---
+++ caesar_cipher.py
@@ -0,0 +1,9 @@
+lowercase_alphabets = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u',
+    'v', 'w', 'x', 'y', 'z'
+]
+msg=input("Type the messege:\n")
+shift=("Enter shift number:\n")
+
