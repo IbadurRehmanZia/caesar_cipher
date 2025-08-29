f245cca60b22bd17647b4264203d40e5c15c1336
unknown <ibadzia3404@gmail.com>
2025-08-29 17:53:49 +0200

    encoding and decoding funtionalities

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,6 +4,23 @@ lowercase_alphabets = [
     'o', 'p', 'q', 'r', 's', 't', 'u',
     'v', 'w', 'x', 'y', 'z'
 ]
+
+def encoder(msg,shift):
+   
+    e_msg=""
+    for a in msg:
+        e_msg+=lowercase_alphabets[lowercase_alphabets.index(a)+shift]
+    
+    return e_msg
+def decoder(msg,shift):
+   
+    d_msg=""
+    for a in msg:
+        d_msg+=lowercase_alphabets[lowercase_alphabets.index(a)-shift]
+    
+    return d_msg
 msg=input("Type the messege:\n")
-shift=("Enter shift number:\n")
+shift=int(input("Enter shift number:\n"))
+# print("encoded messege: ",encoder(msg,shift))
+print("decoded messege: ",decoder(msg,shift))
 
